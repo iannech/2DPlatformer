84cbaecd9349cb3b20bd33fc7e9dbb2bc1491bf4
iannech <ianoniango94@gmail.com>
2015-12-24 16:03:15 +0300

    Added RayCasts for collision detection

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,14 +4,61 @@ using System.Collections;
 [RequireComponent(typeof(BoxCollider2D))]
 public class CharacterController2D : MonoBehaviour {
 
+    const float skinWidth = .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
     BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+
 	void Start () {
 
         collider = GetComponent<BoxCollider2D>();
 	}
+
+    void Update()
+    {
+        // test if rayspacing is calculated correctly
+        updateRayCastOrigins();
+        calculateRaySpacing();
+
+        for(int i = 0; i < verticalRayCount; i++)
+        {
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+    }
+
+    void updateRayCastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void calculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        // calculate ray spacing
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
 	
 	
-	void Update () {
-	
-	}
 }
