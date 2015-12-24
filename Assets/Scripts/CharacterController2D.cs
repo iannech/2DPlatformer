6640f7e36484e5efea1ae6b817b60d6502c563cf
iannech <ianoniango94@gmail.com>
2015-12-24 14:22:01 +0300

    Fisrt commit. Set up Character controller and Player scripts

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour {
+
+    BoxCollider2D collider;
+	void Start () {
+
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour {
+
+    CharacterController2D controller;
+
+	void Start () {
+        controller = GetComponent<CharacterController2D>();
+
+	}
+	
+	
+	void Update () {
+	
+	}
+}
